95568f61cfeaa7c01fe3ab9223bd6c4c025258ea
Jérémie PARDO <Pardo_Jeremie@outlook.com>
2023-08-10 15:30:29 +0200

    Initial Commit

---
+++ chess/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  chess
+//
+//  Created by pardo jérémie on 10/08/2023.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
